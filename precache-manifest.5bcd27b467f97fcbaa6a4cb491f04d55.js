d9c5c318f71ea4f3353893414932af4f03f06c35
Geetansh <f20171079@hyderabad.bits-pilani.ac.in>
2021-03-16 17:41:28 +0530

    Updates

---
+++ precache-manifest.5bcd27b467f97fcbaa6a4cb491f04d55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c59b9c6b373071ea473eb9f79842b09",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ac67a9a7c038174bde3e",
+    "url": "/robofriends/static/css/main.4994eaad.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac67a9a7c038174bde3e",
+    "url": "/robofriends/static/js/main.aa2b0a1f.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
